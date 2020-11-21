817409ff99c7721cc179381196f74ddd10b6611e
Jonti <jontisavani@gmail.com>
2020-11-21 11:23:35 +0530

    first working version

---
+++ script.js
@@ -0,0 +1,26 @@
+replaceText = (node) => {
+  if (node.nodeType === 1 && node.hasAttribute("contenteditable")) {
+    return;
+  } else if (node.hasChildNodes()) {
+    node.childNodes.forEach(replaceText);
+  } else if (node.nodeType === Text.TEXT_NODE) {
+    node.textContent = node.textContent.replace(/hm+/gi, "Got it! Thanks!");
+    node.textContent = node.textContent.replace(/\.+/gi, ".");
+  }
+};
+
+replaceContent = () => {
+  replaceText(document.body);
+};
+
+// only fire when Dom is finished loading
+document.addEventListener("readystatechange", (event) => {
+  if (event.target.readyState === "complete") {
+    // this will observe any/all changes in DOM
+    MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+    const observer = new MutationObserver((mutations, observer) => {
+      replaceContent();
+    });
+    observer.observe(document, { subtree: true, childList: true });
+  }
+});
